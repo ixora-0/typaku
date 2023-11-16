1caafd3c03af4450e7aad6936fc34daf5135f4cd
Harrelix <phunglegend.2004@gmail.com>
2023-11-16 15:25:35 -0500

    fead: add left sidebar.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,13 @@
   import { setModeCurrent, getModeOsPrefers } from "@skeletonlabs/skeleton";
   setModeCurrent(getModeOsPrefers());
   import { LightSwitch } from "@skeletonlabs/skeleton";
+  import { initializeStores } from "@skeletonlabs/skeleton";
+
+  // should be in root layout
+  // required only once when implementing Skeleton's Drawer, Modal, and Toast stores
+  // prevent known issues with SvelteKit SSR
+  // https://www.skeleton.dev/utilities/drawers#prerequisites
+  initializeStores();
 </script>
 
 <LightSwitch />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
   import TypingArea from "$lib/typing/TypingArea.svelte";
   import TypingAreaLoading from "$lib/typing/TypingAreaLoading.svelte";
   import { invoke } from "@tauri-apps/api/tauri";
+  import { Drawer, getDrawerStore, type DrawerSettings } from "@skeletonlabs/skeleton";
 
   async function init_log() {
     await invoke("init_log");
@@ -27,16 +28,43 @@
     typing_area_reset_key = {}; // every {} is unique, {} === {} evaluates to false
     reset_button.blur(); // unfocus on the button, allows for quick resetting
   }
+
+  // must be invoked at the top level
+  // https://www.skeleton.dev/utilities/drawers#drawer-store
+  const drawerStore = getDrawerStore();
+
+  const leftDrawerSettings: DrawerSettings = { width: "w-[280px] md:w-[480px]" };
+  function openRightSidebar(): void {
+    drawerStore.open(leftDrawerSettings);
+  }
 </script>
 
-<button
-  class="variant-ghost-primary bg-primary-hover-token py-2 px-5 text-2xl rounded-token"
-  on:click={init_log}>Init log</button
->
-<button
-  class="variant-ghost-primary bg-primary-hover-token py-2 px-5 text-2xl rounded-token"
-  on:click={load_data}>Load data</button
->
+<Drawer>
+  <h1>This is left sidebar</h1>
+</Drawer>
+
+<button>
+  <button
+    type="button"
+    class="material-symbols-outlined btn-icon mr-4"
+    style="font-size: 24px;"
+    on:click={openRightSidebar}
+  >
+    menu
+  </button>
+</button>
+
+<div>
+  <button
+    class="variant-ghost-primary bg-primary-hover-token py-2 px-5 text-2xl rounded-token"
+    on:click={init_log}>Init log</button
+  >
+  <button
+    class="variant-ghost-primary bg-primary-hover-token py-2 px-5 text-2xl rounded-token"
+    on:click={load_data}>Load data</button
+  >
+</div>
+
 <div class="flex justify-center">
   <LiveWpm />
 </div>
